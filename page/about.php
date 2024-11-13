3cabd73c02ffa28b35383a1fc54651ab692030dd
petrr4 <petraik111@gmail.com>
2024-11-13 17:28:22 +0700

    about n class

---
+++ page/about.php
@@ -0,0 +1,194 @@
+<!-- Header Start -->
+<div class="container-fluid page-header">
+    <div class="container">
+        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 400px">
+            <h3 class="display-4 text-white text-uppercase">About</h3>
+            <div class="d-inline-flex text-white">
+                <p class="m-0 text-uppercase"><a class="text-white" href="inc/..">Home</a></p>
+                <i class="fa fa-angle-double-right pt-1 px-3"></i>
+                <p class="m-0 text-uppercase">About Us</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Header End -->
+
+<!-- About Start -->
+<div class="container-fluid py-5">
+    <div class="container pt-5">
+        <div class="row">
+            <div class="col-lg-6" style="min-height: 500px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute w-100 h-100" src="./assets/img/rajaampat.jpg" style="object-fit: cover;">
+                </div>
+            </div>
+            <div class="col-lg-6 pt-5 pb-lg-5">
+                <div class="about-text bg-white p-4 p-lg-5 my-lg-5">
+                    <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">About Us</h6>
+                    <h2 class="mb-3">Destinesia memberikan pilihan terbaik untuk destinasi wisata kamu dan keluarga</h2>
+                    <p style="text-align: justify;">Destinesia merupakan sebuah Semantic-Website yang berisi informasi mengenai destinasi wisata di Indonesia. 
+                    Destinesia menghadirkan bermacam destinasi wisata dari berbagai provinsi dan pulau di Indonesia dengan informasi yang lengkap dan jelas.
+                    Kami berkomitmen dengan memberikan informasi yang asli dan akurat terhadap setiap destinasi wisata yang kami hadirkan.
+                    Dengan Destinesia kamu tidak perlu bingung lagi untuk menentukan pilihan destinasi wisata yang akan kamu kunjungi.
+                    </p>
+                    <div class="row mb-4">
+                        <div class="col-6">
+                            <img class="img-fluid" src="./assets/img/tanahlot.jpg" style="height: 180px;" alt="">
+                        </div>
+                        <div class="col-6">
+                            <img class="img-fluid" src="./assets/img/borobudur.jpg" style="height: 180px;" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+
+<!-- Feature Start -->
+<div class="container-fluid pt-5">
+    <div class="container pb-4">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="d-flex mb-4 mb-lg-0">
+                    <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-primary mr-3" style="height: 100px; width: 100px;">
+                        <i class="fa fa-2x fa-route text-white"></i>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <h5 class="">Lokasi Yang Akurat</h5>
+                        <p class="m-0">Kami menjelaskan setiap destinasi wisata dengan titik koordinatnya di maps.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="d-flex mb-4 mb-lg-0">
+                    <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-primary mr-3" style="height: 100px; width: 100px;">
+                        <i class="fa fa-2x fa-award text-white"></i>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <h5 class="">Best Services</h5>
+                        <p class="m-0">Kami berkomitmen dalam memberikan data serta informasi yang benar.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="d-flex mb-4 mb-lg-0">
+                    <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-primary mr-3" style="height: 100px; width: 100px;">
+                        <i class="fa fa-2x fa-globe text-white"></i>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <h5 class="">Meyeluruh</h5>
+                        <p class="m-0">Website ini mencakup semua destinasi dari semua pulau di seluruh Indonesia.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Feature End -->
+
+<!-- Team Start -->
+<div class="container-fluid py-5">
+    <div class="container pt-5 pb-3">
+        <div class="text-center mb-3 pb-3">
+            <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Kelompok 4 As Guides</h6>
+            <h1>Our Travel Guides</h1>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
+                <div class="team-item bg-white mb-4">
+                    <div class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="./assets/img/ruth.jpg" alt="">
+                        <div class="team-social">
+                            <a class="btn btn-outline-primary btn-square" href="https://www.instagram.com/ruthgracea_/"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-outline-primary btn-square" href="https://www.linkedin.com/in/ruth-grace-arlyana-manurung-b674b2293/"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <h5 class="text-truncate">Ruth Grace Manurung</h5>
+                        <p class="m-0">Member</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
+                <div class="team-item bg-white mb-4">
+                    <div class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="./assets/img/rifqi.jpg" alt="">
+                        <div class="team-social">
+                            <a class="btn btn-outline-primary btn-square" href="https://www.instagram.com/rifqijabrah/"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-outline-primary btn-square" href="https://www.linkedin.com/in/rifqi-jabrah-b718a0257/"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <h5 class="text-truncate">Rifqi Jabrah Rhae</h5>
+                        <p class="m-0">Member</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
+                <div class="team-item bg-white mb-4">
+                    <div class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="./assets/img/luthfi.jpg" alt="">
+                        <div class="team-social">
+                            <a class="btn btn-outline-primary btn-square" href="https://www.instagram.com/luthfi2603/"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-outline-primary btn-square" href="https://www.linkedin.com/in/muhammad-luthfi-0155a8282/"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <h5 class="text-truncate">Muhammad Luthfi</h5>
+                        <p class="m-0">Member</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
+                <div class="team-item bg-white mb-4">
+                    <div class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="./assets/img/andy.jpg" alt="">
+                        <div class="team-social">
+                            <a class="btn btn-outline-primary btn-square" href="https://www.instagram.com/andysaragih_/"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-outline-primary btn-square" href="https://www.linkedin.com/in/andy-septiawan-saragih-272095255/"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <h5 class="text-truncate">Andy Septiawan Saragih</h5>
+                        <p class="m-0">Member</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
+                <div class="team-item bg-white mb-4">
+                    <div class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="./assets/img/aulia.jpg" alt="">
+                        <div class="team-social">
+                            <a class="btn btn-outline-primary btn-square" href="https://www.instagram.com/rizstiaulia/"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <h5 class="text-truncate">Rizqi Siti Aulia</h5>
+                        <p class="m-0">Member</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
+                <div class="team-item bg-white mb-4">
+                    <div class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="./assets/img/fenaya.jpg" alt="">
+                        <div class="team-social">
+                            <a class="btn btn-outline-primary btn-square" href="https://www.instagram.com/fenayacecilly/"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-outline-primary btn-square" href="https://www.linkedin.com/in/fenaya-cecilly-a760262a0/"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <h5 class="text-truncate">Fenaya Cecilly Nababan</h5>
+                        <p class="m-0">Member</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+<!-- Team End -->
---
+++ page/class.php
@@ -0,0 +1,65 @@
+<?php
+$keyword = $_GET['keyword'];
+
+$query = "
+
+    PREFIX carverse: <http://www.semanticweb.org/brisb/ontologies/2024/10/carverse#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+    SELECT DISTINCT ?name ?class ?thumbnail ?layout WHERE {
+        ?d a carverse:car;
+             rdfs:label           ?name;
+             carverse:carverseclass  ?class;
+             carverse:carversethumbnail ?thumbnail;
+             carverse:carverselayout    ?layout .
+        FILTER(?class = '$keyword') .
+    }
+";
+
+$result = $sparqlJena->query($query);
+?>
+<!-- Header Start -->
+<div class="container-fluid page-header">
+    <div class="container">
+        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 400px">
+            <h3 class="display-4 text-white text-uppercase">Class</h3>
+            <div class="d-inline-flex text-white">
+                <p class="m-0 text-uppercase"><a class="text-white" href="">Home</a></p>
+                <i class="fa fa-angle-double-right pt-1 px-3"></i>
+                <p class="m-0 text-uppercase">Class</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Header End -->
+
+<!-- Blog Start -->
+<div class="container-fluid py-4">
+    <div class="container py-1">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="text-primary">Class: <?= $keyword ?></h2>
+                <div class="row py-3">
+                    <?php foreach ($result as $data) : ?>
+                        <div class="col-lg-4 md-4 mb-4 pb-2">
+                            <div class="blog-item">
+                                <div class="position-relative">
+                                    <img class="img-fluid w-100" src="<?= $data->thumbnail ?>" alt="">
+                                </div>
+                                <div class="bg-white p-4">
+                                    <div class="d-flex mb-2">
+                                        <span class="text-primary text-uppercase text-decoration-none"><?= $data->layout ?></span>
+                                    </div>
+                                    <a class="h5 m-0 text-decoration-none" href="?p=single&keyword=<?= $data->name ?>"><?= $data->name ?></a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- Blog End -->
